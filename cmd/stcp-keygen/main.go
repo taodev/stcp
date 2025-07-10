3ea921735d2f5404996e999bdb84ecf052de6612
taodev <taodev@qq.com>
2025-07-10 23:41:32 +0800

    feat: 支持私钥文件读取与生成及命令行参数配置
    增加通过命令行指定私钥路径或直接传入私钥内容的功能，自动生成和存储私钥，提升易用性和灵活性。

--- cmd/stcp-keygen/main.go
+++ cmd/stcp-keygen/main.go
@@ -4,20 +4,61 @@ import (
 	"crypto/ecdh"
 	"crypto/rand"
 	"encoding/base64"
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
+	keypath := flag.String("f", "id_stcp", "save path")
+	key := flag.String("k", "", "private key")
+	flag.Parse()
+
 	curve := ecdh.X25519()
-	privateKey, err := curve.GenerateKey(rand.Reader)
-	if err != nil {
-		panic(err)
+
+	var privateKey *ecdh.PrivateKey
+	var err error
+
+	if *key != "" {
+		keyBytes, err := base64.RawURLEncoding.DecodeString(*key)
+		if err != nil {
+			fmt.Printf("decode private key error: %v", err)
+			os.Exit(1)
+		}
+		privateKey, err = curve.NewPrivateKey(keyBytes)
+		if err != nil {
+			fmt.Printf("private key error: %v", err)
+			os.Exit(1)
+		}
+	} else if _, err = os.Stat(*keypath); err != nil {
+		privateKey, err = curve.GenerateKey(rand.Reader)
+		if err != nil {
+			fmt.Printf("generate private key error: %v", err)
+			os.Exit(1)
+		}
+		keyBase64 := base64.RawURLEncoding.EncodeToString(privateKey.Bytes())
+		if err = os.WriteFile(*keypath, []byte(keyBase64), 0600); err != nil {
+			fmt.Printf("write private key file error: %v", err)
+			os.Exit(1)
+		}
+	} else {
+		keyString, err := os.ReadFile(*keypath)
+		if err != nil {
+			fmt.Printf("read private key file error: %v", err)
+			os.Exit(1)
+		}
+		keyBase64, err := base64.RawURLEncoding.DecodeString(string(keyString))
+		if err != nil {
+			fmt.Printf("decode private key error: %v", err)
+			os.Exit(1)
+		}
+		privateKey, err = curve.NewPrivateKey(keyBase64)
+		if err != nil {
+			fmt.Printf("read private key file error: %v", err)
+			os.Exit(1)
+		}
 	}
 
-	fmt.Printf("Private Key:\n")
-	fmt.Println(base64.RawURLEncoding.EncodeToString(privateKey.Bytes()))
-
-	publicKey := privateKey.PublicKey()
-	fmt.Printf("Public Key:\n")
-	fmt.Println(base64.RawURLEncoding.EncodeToString(publicKey.Bytes()))
+	pub := privateKey.PublicKey()
+	fmt.Println(base64.RawURLEncoding.EncodeToString(pub.Bytes()))
 }
